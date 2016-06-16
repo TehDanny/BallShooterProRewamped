a078d502847338f2fb510c96824982b57234c8ed
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-16 12:37:56 +0200

    Added class GameController

---
+++ BallShooterProRewamped/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallShooterProRewamped
+{
+    class GameController
+    {
+        Game game = new Game();
+
+        public void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Concede()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndGame()
+        {
+            game.RemoveBalls();
+            game.RemoveBullets();
+            game.PrintWinningStatus();
+        }
+    }
+}
