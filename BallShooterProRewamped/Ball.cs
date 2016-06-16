6ed168e0b925a9c9b9be379cea0d2fcb4fb549be
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-16 12:29:06 +0200

    Added class Bullet

---
+++ BallShooterProRewamped/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallShooterProRewamped
+{
+    class Ball
+    {
+        public Ball()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BallShooterProRewamped/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallShooterProRewamped
+{
+    class Bullet
+    {
+        private int bulletPositionX;
+
+        private int bulletPositionY;
+
+        private int targetPositionX;
+
+        private int targetPositionY;
+
+        private int speed;
+
+        private double angle;
+
+        public Bullet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
