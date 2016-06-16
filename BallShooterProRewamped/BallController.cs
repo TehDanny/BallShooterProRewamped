de6d3a54145637fcd846ac339efdde34cb9d802d
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-16 12:43:09 +0200

    Added class BallController

---
+++ BallShooterProRewamped/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallShooterProRewamped
+{
+    class BallController
+    {
+        public void MoveDirection(string diretion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopMovingDirection(string diretion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
