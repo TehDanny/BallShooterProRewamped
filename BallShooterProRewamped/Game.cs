4b471ca22569e8ba80f76e8cb6f844f2134eddd8
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-16 12:32:59 +0200

    Added class Game

---
+++ BallShooterProRewamped/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallShooterProRewamped
+{
+    class Game
+    {
+        public void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Concede()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RepositionBallsToDefaultPositions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBalls()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBullets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PrintWinningStatus()
+        {
+            throw new NotImplementedException();
+            //string winningStatus = "";
+            //return winningStatus;
+        }
+    }
+}
