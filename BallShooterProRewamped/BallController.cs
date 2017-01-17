9f3814817032efeab498feac1dc64e7589e6085e
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-01-17 14:38:51 +0100

    Added a using threading, a ball object and a constructor

--- BallShooterProRewamped/BallController.cs
+++ BallShooterProRewamped/BallController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace BallShooterProRewamped
 {
     class BallController
     {
+        Ball playerBall;
+
+        public BallController()
+        {
+            playerBall = new Ball();
+        }
+
         public void MoveDirection(string direction)
         {
             throw new NotImplementedException();
